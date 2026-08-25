961eca64f0ed287bb87c8d2f588e4e9c0b75d0d2
Schneidy <schneidy2006@gmail.com>
2026-08-25 12:14:27 -0400

    Add files via upload

---
+++ Airport_departure_Board.py
@@ -0,0 +1,38 @@
+all_departed_flights = []
+#total_passengers = 
+departed_flights = input("How many flights are departing today?: ")
+while departed_flights == (""):
+    print("Nothing?? Then go home.")
+    departed_flights = (input(" How many flights are departing today? :"))
+else:
+    all_departed_flights.append
+    destination = (input("Where do you plan on traveling?: "))
+    while destination == "":
+        print("So are you staying here? Not much of an interesting place...")
+        destination = (input("Where do you plan on traveling?: "))
+
+
+
+
+#if destination == "":
+     #print("What? You wanna stay home or something?")
+     #destination = (input("Where do you plan on traveling?: "))
+
+
+
+
+
+#passenger_count = int(input("How many passengers are boarding this flight?: "))  
+#if passenger_count <= 1:
+    #print("Are you doing a test flight? Where are the passengers?")
+#elif passenger_count >= 300:
+    #print("Unless you want people to stand on the flight, you're gonna have to shed some weight")
+
+#print("=========================")
+#print("DEPARTURE BOARD")
+#print("=========================")
+
+    #print("Please enter a number of flights")
+    #departed_flights = int(input(" How many flights are departing today? :"))
+
+
