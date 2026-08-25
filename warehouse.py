33e7c66e626d5982f3e9bd6c2ffa09ae890083d1
Schneidy <schneidy2006@gmail.com>
2026-08-25 12:13:08 -0400

    Add files via upload

---
+++ warehouse.py
@@ -0,0 +1,14 @@
+shelves = 8 
+storage_bins = 5 
+
+
+for shelf in range(shelves):
+    print()
+    for bin in range(storage_bins,):
+        if (bin + 1) % 2 == 1:
+            print(f"Shelf {shelf + 1} -  Bin {bin + 1}: FAIL ")
+        else:
+             print(f"Shelf {shelf + 1} -  Bin {bin + 1}: PASS")
+        
+        
+        #what about this??
